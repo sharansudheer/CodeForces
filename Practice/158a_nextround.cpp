b429ea3b106ea446055d69898a82c605ca187bd1
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-08-21 12:28:52 +0530

    Create 158a_nextround.cpp

---
+++ Practice/158a_nextround.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+
+  int a,key;
+  cin>>a>>key;
+  int arr[a];
+  cout<<"Enter the elements";
+  for(int i=0; i<a; i++){
+    cin>>arr[n];
+   } 
+  int n=sizeof(arr)/sizeof(int);
+   
+   
+   // Binary Search function
+  auto it=upper_bound(arr,arr-a,key);
+  return 0;
+}
